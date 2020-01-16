038181d23d18253d901140bb370b40338f11ba04
JesusBetaX <jjbaezmm@yahoo.com>
2020-01-15 18:18:16 -0600

    es

---
+++ private/database/migrations/2020_01_16_000714_create_empresas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('razon_social')->nullable(false);
+            $table->string('credito_contado')->nullable(false);
+            $table->integer('dias_credito')->nullable(false);
+            $table->string('tipo_cuenta')->nullable(false);
+            $table->string('clasificacion')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresa');
+    }
+}
